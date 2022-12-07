ed5c04c49e3b8d45d668ec09b0e2630e3e5d89bf
RJDowding <dowd.reece@gmail.com>
2022-12-07 13:59:53 +0000

    POST works.

--- ca4/index.js
+++ ca4/index.js
@@ -2,15 +2,18 @@ const express = require("express");
 const socket = require("socket.io");
 const argon2 = require("argon2");
 const fs = require("fs");
-const { response } = require("express");
 const router = express.Router();
 
 // App setup
 const PORT = 4000;
 const app = express();
+// Sive through request to find data.
+app.post('/login.html', function(req, res) {
+  res.send('POSTING!!!!')
+});
 const server = app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-
+  console.log(`Listening on port ${PORT}` + " Relative path is: " + __dirname);
+  
 });
 
 // Static files
@@ -24,34 +27,29 @@ const activeUsers = new Set();
 
 io.on("connection", function (socket) {
   console.log("Made socket connection");
-
+  
   socket.on("new user", function (data) {
     socket.userId = data;
     activeUsers.add(data);
     //... is the the spread operator, adds to the set while retaining what was in there already
     io.emit("new user", [...activeUsers]);
   });
-
+  
   socket.on("disconnect", function () {
-      activeUsers.delete(socket.userId);
-      io.emit("user disconnected", socket.userId);
-    });
-
-    socket.on("chat message", function (data) {
-      io.emit("chat message", data);
+    activeUsers.delete(socket.userId);
+    io.emit("user disconnected", socket.userId);
   });
-
+  
+  socket.on("chat message", function (data) {
+    io.emit("chat message", data);
+  });
 });
-// Sive through request to find data.
-router.get('/sign-up', (request, response) => {
-  console.log(request, response)
-})
 
 async function create_user(email, password) {
   try {
     // Hashes are salted automatically.
     const p_hash = await argon2.hash(password);
-    const e_hash = await argon2.hash(email)
+    const e_hash = await argon2.hash(email);
     write_file(e_hash, p_hash); 
   } catch(err) {
     // Create web popup for errors
@@ -63,8 +61,8 @@ async function login_user(entered_email, entered_password) {
   //user_list = read_file();
   var user_list = ["a@b.com", "pword"];
   if (entered_email == user_list[1]) {
-    console.log('MATCHING')
-    return
+    console.log('MATCHING');
+    return;
   }
   for (var index in user_list) {
     try {
